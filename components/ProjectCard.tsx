1076b8d8102d5a5b174060dddae774b61b65a01c
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-02 22:07:14 +0300

    update ProjectCard.tsx

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -8,17 +8,19 @@ interface Props {
 const ProjectCard: React.FC<Props> = ({ project }) => {
   return (
     <>
-      <div className="flex flex-1 w-full items-center  justify-center relative">
+      <div className="flex flex-1 w-full items-center group justify-center relative">
         <img
           // src={`/projects/${project.image}`}
           src={`/${project.image}`}
-          className="absolute inset-0 w-full h-full object-cover group"
+          className="absolute inset-0 w-full h-full object-cover"
           alt=""
         />
-        <div className="group-hover:block hidden bg-black backdrop-brightness-50 inset-0 absolute"></div>
-        <h2 className="group-hover:block hidden absolute self-center font-bold text-3xl">
-          {project.name}
-        </h2>
+        <div className="group-hover:opacity-100 opacity-0 duration-500">
+          <div className="bg-black bg-opacity-50 inset-0 absolute"></div>
+          <h2 className="absolute self-center font-bold text-3xl">
+            {project.name}
+          </h2>
+        </div>
       </div>
     </>
   );
